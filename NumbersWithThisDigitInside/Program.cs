427230f1147e11e4cf703e5f1d9408a067e5de45
fasdser <107477645+fasdser@users.noreply.github.com>
2022-07-11 21:57:41 +0300

    Add project files.

---
+++ NumbersWithThisDigitInside/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersWithThisDigitInside
+{
+    internal class Program
+    {
+        /* You have to search all numbers from inclusive 1 to inclusive a given number x, that have the given digit d in it.
+ The value of d will always be 0 - 9.
+ The value of x will always be greater than 0.
+
+ You have to return as an array
+
+ the count of these numbers,
+ their sum
+ and their product.
+
+ For example:
+ x = 11
+ d = 1
+ ->
+ Numbers: 1, 10, 11
+ Return: [3, 22, 110]
+
+         If there are no numbers, which include the digit, return [0,0,0].
+
+ Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+ I have created other katas.Have a look if you like coding and challenges.*/
+        /* Вы должны искать все числа от 1 включительно до заданного числа x включительно, в котором есть заданная цифра d.
+ Значение d всегда будет от 0 до 9.
+ Значение x всегда будет больше 0.
+
+ Вы должны вернуть как массив
+
+ количество этих чисел,
+ их сумма
+ и их продукт.
+
+ Например:
+ х = 11
+ д = 1
+ ->
+ Числа: 1, 10, 11
+ Возврат: [3, 22, 110]
+
+          Если нет чисел, включающих цифру, вернуть [0,0,0].
+
+ Получайте удовольствие от его кодирования и, пожалуйста, не забудьте проголосовать и оценить этот ката! :-)
+
+ Я создал и другие ката. Посмотрите, если вам нравится программирование и вызовы.*/
+        static void Main(string[] args)
+        {
+            NumbersWithDigitInside(11, 1);
+            Console.WriteLine(NumbersWithDigitInside(11,1));
+            Console.ReadLine();
+        }
+
+        
+            public static long[] NumbersWithDigitInside(long x, long d)
+            {
+                long count = 0;
+                long sum = 0;
+                long product = 1;
+
+                for (long i = 1; i <= x; i++)
+                {
+                    if (i.ToString().Contains(d.ToString()))
+                    {
+                        count += 1;
+                        sum += i;
+                        product *= i;
+                    }
+                }
+                if (count == 0)
+                {
+                    product = 0;
+                }
+                return new long[] { count, sum, product };
+            }
+
+        public static long[] NumbersWithDigitInside1(long x, long d)
+        {
+            var numbers = new List<long>();
+            for (long i = 1; i <= x; i++)
+            {
+                if (i.ToString().Contains(d.ToString()))
+                {
+                    numbers.Add(i);
+                }
+            }
+            if (numbers.Count == 0)
+            {
+                return new long[] { 0, 0, 0 };
+            }
+            return new long[] { numbers.Count, numbers.Sum(), numbers.Aggregate((a, b) => a * b) };
+        }
+    }
+    }
+
---
+++ NumbersWithThisDigitInside/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NumbersWithThisDigitInside")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NumbersWithThisDigitInside")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d7d39af7-56ba-41c3-866d-70b3be37b418")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
